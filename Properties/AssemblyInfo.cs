5cd98ba4a82986807dac2445069ad2cedafbb7b3
Underscore76 <theunderscore76@gmail.com>
2021-10-15 09:56:26 -0500

    helps to bump the version?

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using LiveSplit.UI.Components;
 using System.Reflection;
 
 [assembly: AssemblyTitle("LiveSplit.StardewValley")]
-[assembly: AssemblyVersion("1.2.0")]
-[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyVersion("1.2.1")]
+[assembly: AssemblyFileVersion("1.2.1")]
 [assembly: ComponentFactory(typeof(Factory))]
