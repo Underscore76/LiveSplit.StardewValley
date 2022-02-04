cddf2c07af5f1f16d9b3c5f946475604375e22df
Underscore76 <theunderscore76@gmail.com>
2022-02-03 18:20:57 -0600

    paths updated to move file correctly

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using LiveSplit.UI.Components;
 using System.Reflection;
 
 [assembly: AssemblyTitle("LiveSplit.StardewValley")]
-[assembly: AssemblyVersion("1.2.4")]
-[assembly: AssemblyFileVersion("1.2.4")]
+[assembly: AssemblyVersion("1.2.5")]
+[assembly: AssemblyFileVersion("1.2.5")]
 [assembly: ComponentFactory(typeof(Factory))]
